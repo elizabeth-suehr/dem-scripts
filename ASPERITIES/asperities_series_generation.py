1c8b3110f56fbd1ee6647b1f1dca76d4178781c6
elizabeth-suehr <77520680+elizabeth-suehr@users.noreply.github.com>
2025-04-06 00:41:12 -0700

    asperities longer relaxation run

--- ASPERITIES/asperities_series_generation.py
+++ ASPERITIES/asperities_series_generation.py
@@ -104,7 +104,7 @@ def asperities_series_validate_fortran():
 
 
 def asperities_series_validate_all():
-    my_list = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+    my_list = [1, 2, 3, 4, 5, 6, 7, 8]
 
     all_simuations = []
     for i in my_list:
